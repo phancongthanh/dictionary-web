1b948c7ad83b4fb48d48f3a5e8cb84b3f216ca69
Phan Cong Thanh <phancongthanhvtvpvn@gmail.com>
2022-05-13 14:08:24 +0700

    Upload pronunciation view

---
+++ public/javascripts/pronunciation.js
@@ -0,0 +1,36 @@
+async function putSpeech(target, pronunciation) {
+    console.log({target, pronunciation})
+    await fetch('/pronunciation', {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({target, pronunciation})
+    })
+    location.assign('/word/view?target='+target);
+}
+
+function addLetter_click(element) {
+    const input = document.getElementById("pronInput");
+    input.value += element.innerText;
+}
+
+function deleteLetter_click() {
+    const input = document.getElementById("pronInput");
+    input.value = input.value.substr(0, input.value.length - 1);
+}
+
+function saveClick() {
+    const div = document.getElementById("target");
+    const input = document.getElementById("pronInput");
+
+    const target = div.innerText;
+    const pronunciation = input.value;
+    putSpeech(target, pronunciation)
+}
+
+function deleteClick() {
+    const div = document.getElementById("target");
+    const target = div.innerText;
+    putSpeech(target, "")
+}
